4fb1027baccd3d3a663100f29b7eb27e23e1096c
nikikotak1211 <nikitakotak12@gmail.com>
2018-01-22 11:18:11 +0530

    Updated

---
+++ Session1_21Jan/ScarnesDice_App_UI/app/src/main/java/com/example/nikita/scarnesdice/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.nikita.scarnesdice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.Random;
+
+
+public class MainActivity extends AppCompatActivity {
+
+   
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+
+}
